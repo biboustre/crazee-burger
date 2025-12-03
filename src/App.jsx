08892c186b4be6b47d715ce267cb8605d407c0b1
Habib <biboustre@hotmail.com>
2025-12-03 20:54:12 +0100

    ✨F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import "./App.css";
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
   // state (état, données)
-  const [] = useState();
   // Comportement (fonctions, effets)
 
   // affichage (render)
   return (
-    <main>
-      <h1>Bienvenue chez nous</h1>
-      <h2>Connectez-vous </h2>
-    </main>
+    <LoginPage />
   );
 }
 
