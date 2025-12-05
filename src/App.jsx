938b4b475fd419bfac737d5d507914599e862532
Habib <biboustre@hotmail.com>
2025-12-05 01:07:04 +0100

    ✨F01: LOGIN > Add title and form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from "./components/pages/LoginPage";
 
 function App() {
   // state (état, données)
+  
   // Comportement (fonctions, effets)
 
   // affichage (render)
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+  // State
+  const [prenom, setPrenom] = useState("");
+  //   Comportement
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bienvenue, ${prenom} !`);
+    setPrenom("");
+  };
+
+    const handleChange = (e) => {
+    setPrenom(e.target.value);
+  };
+
+
   return (
-    <div>LoginPage</div>
-  )
+    <main>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          value={prenom}
+          onChange={handleChange}
+          required
+        />
+        <button>Accéder à son espace</button>
+      </form>
+    </main>
+  );
 }
