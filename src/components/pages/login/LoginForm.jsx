b897b82804d084d75734503c1c80cde404338a94
Habib <biboustre@hotmail.com>
2025-12-05 01:20:56 +0100

    ♻️F01: LOGIN > Create LoginForm

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+      // State
+  const [prenom, setPrenom] = useState("");
+  //   Comportement
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bienvenue, ${prenom} !`);
+    setPrenom("");
+  };
+
+  const handleChange = (e) => {
+    setPrenom(e.target.value);
+  };
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <input
+        type="text"
+        placeholder="Entrez votre prénom..."
+        value={prenom}
+        onChange={handleChange}
+        required
+      />
+      <button>Accéder à son espace</button>
+    </form>
+  );
+}
