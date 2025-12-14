03b641d20ed80e4980430c2915c8b6e57252a102
Habib <biboustre@hotmail.com>
2025-12-14 01:31:56 +0100

    ✨F02: APP > install routing systeme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/order/OrderPage";
+import ErrorPage from "./components/pages/error/ErrorPage";
 
 function App() {
   // state (état, données)
-  
+
   // Comportement (fonctions, effets)
 
   // affichage (render)
   return (
-    <LoginPage />
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
 
