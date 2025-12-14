b926211c5f02669c439f04e99b0df4518136b74d
Habib <biboustre@hotmail.com>
2025-12-14 01:47:41 +0100

    ✨F02: LOGIN > Add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -5,6 +5,7 @@ export default function ErrorPage() {
   return (
     <section>
       <h1>ErrorPage</h1>
+      <Link to="/"><button>Retour vers la page d'accueil</button></Link>
     </section>
   )
 }
