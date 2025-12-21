34a595589444a3824566b67fc719b083449a6cfc
Habib <biboustre@hotmail.com>
2025-12-21 16:31:12 +0100

    🔥F03: LoginForm > Reset LoginForm after cours styledComponent

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+// import styled from "styled-components/macro"; "macro" permet d'avoir des noms de classes plus parlants dans le devtools
 
 export default function LoginForm() {
   // State
@@ -16,18 +18,18 @@ export default function LoginForm() {
     setInputValue(e.target.value);
   };
   return (
-    <form action="submit" onSubmit={handleSubmit}>
+    <div action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
       <input
         type="text"
-        placeholder="Entrez votre prénom..."
+        placeholder="Entrez votre prénom"
         value={inputValue}
         onChange={handleChange}
         required
       />
-      <button>Accéder à son espace</button>
-    </form>
+      <button>Accéder à mon espace</button>
+    </div>
   );
 }
