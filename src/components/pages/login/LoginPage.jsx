1b4e155c27ecdbc16c8b80a1d4cfd151481f16cb
Habib <biboustre@hotmail.com>
2025-12-21 21:30:06 +0100

    💄F03: LOGIN > Add styling and background images

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -13,10 +13,22 @@ export default function LoginPage() {
 
 // Styled Components, pour le style de la page de connexion
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center; 
+  align-items: center;
+
+  ::before {
+    content: "";
+    background: url("/images/login/burger-background.jpg") no-repeat
+      center/cover;
+    filter: brightness(30%);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
 `;
