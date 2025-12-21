6b75b8bef6cf9a1d20019ae4579a6772e448bd7d
Habib <biboustre@hotmail.com>
2025-12-21 21:04:44 +0100

    💄F03: LoginPage > add layout

---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Logo() {
+  return <LogoStyled>Logo</LogoStyled>;
+}
+
+// Styled Components, pour le style du logo
+const LogoStyled = styled.div`
+  background: blue;
+`;
