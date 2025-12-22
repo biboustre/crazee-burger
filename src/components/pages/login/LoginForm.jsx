81c3ee6f4130e72dd1483291632b79930c63e05c
Habib <biboustre@hotmail.com>
2025-12-22 10:06:27 +0100

    💄F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { theme } from "../../../theme/index.js";
 // import styled from "styled-components/macro"; "macro" permet d'avoir des noms de classes plus parlants dans le devtools
 
 export default function LoginForm() {
@@ -20,7 +21,7 @@ export default function LoginForm() {
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <section className="container-btn-input">
         <input
@@ -39,6 +40,29 @@ export default function LoginForm() {
 // Styled Components, pour le style du formulaire de connexion
 const LoginFormStyled = styled.form`
   background: green;
+  text-align: center;
+  padding: 2.5rem 2rem;
+  color: white;
+  border-radius: 10px;
+  max-width: 500px;
+  min-width: 400px;
+  font-family: "Amatic SC", cursive;
+
+  h1 {
+    font-size: ${theme.fonts.P5};
+    margin-bottom: 40px;
+  }
+
+  h2{
+    font-size: ${theme.fonts.P4};
+    margin: 20px 10px 10px;
+  }
+
+  hr{
+    /* height: 3px;
+    background-color:; */
+    border: 1.5px solid ${theme.colors.primary_burger};
+  }
 
   .container-btn-input {
     display: flex;
