044d821dc46ea17bb1383e44bdc43c983507678b
Habib <biboustre@hotmail.com>
2025-12-22 09:42:28 +0100

    💄F03: LOGO > Add styling

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,10 +1,40 @@
 import styled from "styled-components";
+import { theme } from "../../theme";
 
 export default function Logo() {
-  return <LogoStyled>Logo crazee burger</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h1>Crazee</h1>
+      <img src="/images/logo-burger.png" alt="Logo-crazee-burger" />
+      <h1>Burger</h1>
+    </LogoStyled>
+  );
 }
 
 // Styled Components, pour le style du logo
 const LogoStyled = styled.div`
-  background: blue;
+  border: 1px solid blue;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  /* transform: scale(2.5); */
+
+  h1 {
+    display: inline;
+    text-align: center;
+    text-transform: uppercase;
+    color: ${theme.colors.primary_burger};
+    font-family: "Amatic SC", cursive;
+    font-weight: ${theme.weights.bold};
+    font-size: ${theme.fonts.P6};
+    /* line-height: 1em; */
+    letter-spacing: 1.5px;
+  }
+
+  img {
+    /* height: 60px; */
+    /* object-fit: contain;
+    object-position: center; */
+    width: 200px;
+  }
 `;
